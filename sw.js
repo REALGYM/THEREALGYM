6b2b1d86268f0ed4c3a45577a8f64c361f70888b
REALGYM <rikystraf@gmail.com>
2026-09-22 18:50:31 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Installato');
-});
-
-self.addEventListener('fetch', (e) => {
-  // Gestione standard delle richieste di rete
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-});
